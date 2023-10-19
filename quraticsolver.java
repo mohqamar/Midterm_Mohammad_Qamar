80ce5b8d0572fdec77257549bec800add6ad00f9
mohqamar <mqamar@umichm.edu>
2023-10-19 16:40:55 -0400

    testcommit

--- quraticsolver.java
+++ quraticsolver.java
@@ -5,7 +5,7 @@ public class quraticsolver {
 		// TODO Auto-generated method stub
 		int a = 7;
 		int b = 7;
-		int c = 7;
+		int c = 8;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
